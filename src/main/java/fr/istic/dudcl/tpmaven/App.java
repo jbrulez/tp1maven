da6abd507df834b935f535db34ac976c75d7042e
Jimmy Brulez <jbrulez@deltadore.com>
2016-03-30 16:01:25 +0200

    1er commit du projet

---
+++ src/main/java/fr/istic/dudcl/tpmaven/App.java
@@ -0,0 +1,13 @@
+package fr.istic.dudcl.tpmaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
