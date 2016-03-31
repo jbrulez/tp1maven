4816838cc3c8a9fa117fd88fc5e1a0a460d30b06
Jimmy Brulez <jbrulez@deltadore.com>
2016-03-31 10:07:57 +0200

    ajout de 3 ligne de code

--- src/main/java/fr/istic/dudcl/tpmaven/App2.java
+++ src/main/java/fr/istic/dudcl/tpmaven/App2.java
@@ -1,7 +1,11 @@
 package fr.istic.dudcl.tpmaven;
 
 public class App2 {
+	
 	public void test(){
+		System.out.println("hello world Simon");
+		System.out.println("hello world Simon");
+		System.out.println("hello world Simon");
 		System.out.println("hello world Simon");
 	}
 
